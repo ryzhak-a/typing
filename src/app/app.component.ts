c2ad2cc30ebe69f5f6b6215f88c3e587544316c3
Andrii Ryzhak <55483532+ryzhak-andrii@users.noreply.github.com>
2020-10-07 10:04:21 +0300

    Finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string): void {
+    this.enteredText = value;
+  }
+
+  compare(generatedLetter: string, enteredLetter: string): string {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return generatedLetter === enteredLetter
+      ? 'correct'
+      : 'incorrect';
+  }
 }
